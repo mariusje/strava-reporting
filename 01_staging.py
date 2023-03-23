ba5999bb66be94b5879968fc0da2901334145a3e
mariusje@gmail.com <mariusje@gmail.com>
2023-03-23 11:32:50 +0000

    Første commit.

---
+++ 01_staging.py
@@ -0,0 +1,221 @@
+# Databricks notebook source
+pip install requests
+
+# COMMAND ----------
+
+spark.sql("CREATE CATALOG IF NOT EXISTS strava_catalog")
+spark.sql("USE CATALOG strava_catalog")
+spark.sql("GRANT CREATE SCHEMA, CREATE TABLE, CREATE VIEW, USE CATALOG ON CATALOG strava_catalog TO `account users`")
+
+# COMMAND ----------
+
+spark.sql("CREATE SCHEMA IF NOT EXISTS strava_stage_schema COMMENT \"A new Unity Catalog schema called strava_stage_schema\"")
+spark.sql("USE strava_stage_schema")
+
+# COMMAND ----------
+
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+athlete_url = 'https://www.strava.com/api/v3/athlete'
+
+payload = {
+    'client_id': "78457",
+    'client_secret': 'd33044b3258b6ae45e8000579cf683abd821b746',
+    'refresh_token': '20dab2507bade463981b9604eca00d406af57924',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+
+header = {'Authorization': 'Bearer ' + access_token}
+
+# COMMAND ----------
+
+import json
+
+my_athlete = requests.get(athlete_url, headers=header).json()
+
+my_athlete_json = json.dumps(my_athlete)
+my_athlete_json_data_list = []
+my_athlete_json_data_list.append(my_athlete_json)
+json_rdd = sc.parallelize(my_athlete_json_data_list)
+my_athlete_df = spark.read.json(json_rdd)
+type(my_athlete_df)
+
+my_athlete_df.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_athlete")
+
+# COMMAND ----------
+
+request_page_num = 1
+all_activities = []
+
+while True:
+    param = {'per_page': 200, 'page': request_page_num}
+    my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+    if len(my_dataset) == 0:
+        print("breaking out of while loop because the response is zero, which means there must be no more activities")
+        break
+
+    if all_activities:
+        print("all_activities is populated")
+        all_activities.extend(my_dataset)
+
+    else:
+        print("all_activities is NOT populated")
+        all_activities = my_dataset
+
+    request_page_num += 1
+
+my_dataset = all_activities
+
+# COMMAND ----------
+
+from pandas.io.json import json_normalize
+
+# COMMAND ----------
+
+activities = json_normalize(my_dataset)
+
+# COMMAND ----------
+
+cols = [
+    'resource_state',
+    'name',
+    'distance',
+    'moving_time',
+    'elapsed_time',
+    'total_elevation_gain',
+    'type',
+    'sport_type',
+    'id',
+    'start_date',
+    'start_date_local',
+    'timezone',
+    'utc_offset',
+    'location_city',
+    'location_state',
+    'location_country',
+    'achievement_count',
+    'kudos_count',
+    'comment_count',
+    'athlete_count',
+    'photo_count',
+    'trainer',
+    'commute',
+    'manual',
+    'private',
+    'visibility',
+    'flagged',
+    'gear_id',
+    'start_latlng',
+    'end_latlng',
+    'average_speed',
+    'max_speed',
+    'average_cadence',
+    'average_temp',
+    'has_heartrate',
+    'average_heartrate',
+    'max_heartrate',
+    'heartrate_opt_out',
+    'display_hide_heartrate_option',
+    'elev_high',
+    'elev_low',
+    'upload_id',
+    'upload_id_str',
+    'external_id',
+    'from_accepted_tag',
+    'pr_count',
+    'total_photo_count',
+    'has_kudoed',
+    'suffer_score',
+    'athlete.id',
+    'athlete.resource_state',
+    'map.id',
+    'map.summary_polyline',
+    'map.resource_state',
+    'average_watts',
+    'max_watts',
+    'weighted_average_watts',
+    'kilojoules',
+    'device_watts',
+    'workout_type'
+]
+activities = activities[cols]
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+schema = StructType([
+    StructField("resource_state",IntegerType(),True),
+    StructField("name",StringType(),True),
+    StructField("distance",FloatType(),True),
+    StructField("moving_time",IntegerType(),True),
+    StructField("elapsed_time",IntegerType(),True),
+    StructField("total_elevation_gain",FloatType(),True),
+    StructField("type",StringType(),True),
+    StructField("sport_type",StringType(),True),
+    StructField("id",IntegerType(),True),
+    StructField("start_date",StringType(),True),
+    StructField("start_date_local",StringType(),True),
+    StructField("timezone",StringType(),True),
+    StructField("utc_offset",FloatType(),True),
+    StructField("location_city",StringType(),True),
+    StructField("location_state",StringType(),True),
+    StructField("location_country",StringType(),True),
+    StructField("achievement_count",IntegerType(),True),
+    StructField("kudos_count",IntegerType(),True),
+    StructField("comment_count",IntegerType(),True),
+    StructField("athlete_count",IntegerType(),True),
+    StructField("photo_count",IntegerType(),True),
+    StructField("trainer",BooleanType(),True),
+    StructField("commute",BooleanType(),True),
+    StructField("manual",BooleanType(),True),
+    StructField("private",BooleanType(),True),
+    StructField("visibility",StringType(),True),
+    StructField("flagged",BooleanType(),True),
+    StructField("gear_id",IntegerType(),True),
+    StructField("start_latlng",ArrayType(FloatType(),True),True),
+    StructField("end_latlng",ArrayType(FloatType(),True),True),
+    StructField("average_speed",FloatType(),True),
+    StructField("max_speed",FloatType(),True),
+    StructField("average_cadence",FloatType(),True),
+    StructField("average_temp",FloatType(),True),
+    StructField("has_heartrate",BooleanType(),True),
+    StructField("average_heartrate",FloatType(),True),
+    StructField("max_heartrate",FloatType(),True),
+    StructField("heartrate_opt_out",BooleanType(),True),
+    StructField("display_hide_heartrate_option",BooleanType(),True),
+    StructField("elev_high",FloatType(),True),
+    StructField("elev_low",FloatType(),True),
+    StructField("upload_id",IntegerType(),True),
+    StructField("upload_id_str",StringType(),True),
+    StructField("external_id",StringType(),True),
+    StructField("from_accepted_tag",BooleanType(),True),
+    StructField("pr_count",IntegerType(),True),
+    StructField("total_photo_count",IntegerType(),True),
+    StructField("has_kudoed",BooleanType(),True),
+    StructField("suffer_score",FloatType(),True),
+    StructField("athlete.id",IntegerType(),True),
+    StructField("athlete.resource_state",IntegerType(),True),
+    StructField("map.id",StringType(),True),
+    StructField("map.summary_polyline",StringType(),True),
+    StructField("map.resource_state",IntegerType(),True),
+    StructField("average_watts",FloatType(),True),
+    StructField("max_watts",FloatType(),True),
+    StructField("weighted_average_watts",FloatType(),True),
+    StructField("kilojoules",FloatType(),True),
+    StructField("device_watts",BooleanType(),True),
+    StructField("workout_type",FloatType(),True)
+])
+
+activities_df = spark.createDataFrame(activities, schema)
+
+activities_df.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_activity")
---
+++ 02_mart.py
@@ -0,0 +1,219 @@
+# Databricks notebook source
+spark.sql("USE CATALOG strava_catalog")
+spark.sql("CREATE SCHEMA IF NOT EXISTS strava_mart_schema COMMENT \"A new Unity Catalog schema called strava_mart_schema\"")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import when, col
+
+na_string = "N/A"
+
+spark.sql("USE strava_stage_schema")
+
+activity = spark.read.table("dbtestzwift_activity")
+
+activity = (
+    activity.withColumn(
+        "type",
+        when(col("type").isNull(), na_string).otherwise(col("type"))
+    )
+)
+    
+activity = (
+    activity.withColumn(
+        "sport_type",
+        when(col("sport_type").isNull(), na_string).otherwise(col("sport_type"))
+    )
+)
+    
+activity = (
+    activity.withColumn(
+        "location_city",
+        when(col("location_city").isNull(), na_string).otherwise(col("location_city"))
+    )
+)
+    
+activity = (
+    activity.withColumn(
+        "location_state",
+        when(col("location_state").isNull(), na_string).otherwise(col("location_state"))
+    )
+)
+    
+activity = (
+    activity.withColumn(
+        "location_state",
+        when(col("location_state").isNull(), na_string).otherwise(col("location_state"))
+    )
+)
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number
+
+# COMMAND ----------
+
+spark.sql("USE strava_mart_schema")
+
+# COMMAND ----------
+
+dim_sport_type = (
+    activity
+    .select("type","sport_type")
+    .distinct()
+)
+
+window = Window().orderBy("type","sport_type")
+dim_sport_type = dim_sport_type.withColumn("row_number",row_number().over(window))
+dim_sport_type = dim_sport_type.select(col("row_number").alias("sk_sport_type"),col("type").alias("type"),col("sport_type").alias("sport_type"))
+
+dim_sport_type.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_dim_sport_type")
+
+# COMMAND ----------
+
+dim_geography = (
+    activity
+    .select("location_city","location_state","location_country")
+    .distinct()
+)
+
+window = Window().orderBy("location_country","location_state","location_city")
+dim_geography = dim_geography.withColumn("row_number",row_number().over(window))
+dim_geography = dim_geography.select(col("row_number").alias("sk_geography"),col("location_city").alias("city"),col("location_state").alias("state"),col("location_country").alias("country"))
+
+dim_geography.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_dim_geography")
+
+# COMMAND ----------
+
+import pandas as pd
+
+start_date = '2009-01-01'
+end_date = '2030-12-31'
+
+date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+
+df = pd.DataFrame({'date': date_range})
+df['sk_time'] = df['date'].dt.strftime('%Y%m%d')
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+
+df = df.drop('date', axis=1)
+
+dim_time = spark.createDataFrame(df)
+
+dim_time.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_dim_time")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import year,month,dayofmonth,hour,minute,second
+
+ftr_activity_tmp = (
+    activity
+    .select(
+        "id",
+        "distance",
+        "moving_time",
+        "elapsed_time",
+        "total_elevation_gain",
+        "comment_count",
+        "athlete_count",
+        "photo_count",
+        "average_speed",
+        "max_speed",
+        "average_cadence",
+        "average_temp",
+        "average_heartrate",
+        "max_heartrate",
+        "total_photo_count",
+        "suffer_score",
+        "average_watts",
+        "max_watts",
+        "weighted_average_watts",
+        "kilojoules",
+        "type",
+        "sport_type",
+        "location_city",
+        "location_state",
+        "location_country",
+        "start_date_local"
+    )
+)
+
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_year", year("start_date_local"))
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_month", month("start_date_local"))
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_day", dayofmonth("start_date_local"))
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_hour", hour("start_date_local"))
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_minute", minute("start_date_local"))
+ftr_activity_tmp = ftr_activity_tmp.withColumn("start_second", second("start_date_local"))
+
+ftr_activity_tmp2 = ftr_activity_tmp.join(
+    dim_sport_type,
+    (
+        (ftr_activity_tmp.type == dim_sport_type.type) &
+        (ftr_activity_tmp.sport_type == dim_sport_type.sport_type)
+    ),
+    "inner"
+)
+
+ftr_activity_tmp3 = ftr_activity_tmp2.join(
+    dim_geography,
+    (
+        (ftr_activity_tmp2.location_city == dim_geography.city) &
+        (ftr_activity_tmp2.location_state == dim_geography.state) &
+        (ftr_activity_tmp2.location_country == dim_geography.country)
+    ),
+    "inner"
+)
+
+ftr_activity_tmp4 = ftr_activity_tmp3.join(
+    dim_time,
+    (
+        (ftr_activity_tmp3.start_year == dim_time.year) &
+        (ftr_activity_tmp3.start_month == dim_time.month) &
+        (ftr_activity_tmp3.start_day == dim_time.day)
+    ),
+    "inner"
+)
+
+ftr_activity = (
+    ftr_activity_tmp4
+    .select(
+        "id",
+        "distance",
+        "moving_time",
+        "elapsed_time",
+        "total_elevation_gain",
+        "comment_count",
+        "athlete_count",
+        "photo_count",
+        "average_speed",
+        "max_speed",
+        "average_cadence",
+        "average_temp",
+        "average_heartrate",
+        "max_heartrate",
+        "total_photo_count",
+        "suffer_score",
+        "average_watts",
+        "max_watts",
+        "weighted_average_watts",
+        "kilojoules",
+        "start_hour",
+        "start_minute",
+        "start_second",
+        "sk_sport_type",
+        "sk_geography",
+        "sk_time"
+    )
+)
+
+ftr_activity.write.format("delta").mode("overwrite").saveAsTable("dbtestzwift_ftr_activity")
+
+# COMMAND ----------
+
+#spark.sql("drop table if exists dbtestzwift_dim_geography")
+#spark.sql("drop table if exists dbtestzwift_dim_sport_type")
+#spark.sql("drop table if exists dbtestzwift_dim_time")
+#spark.sql("drop table if exists dbtestzwift_ftr_activity")
